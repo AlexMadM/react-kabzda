f7f3c50dcf52585903aaf98deb0a1dfea102218e
Alex <alex.alexandrov.1988@gmail.com>
2023-09-28 23:33:03 +0300

    lesson-5

---
+++ src/Accordion.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+type AccordionPropsType={
+    titleValue:string
+    collapsed:boolean
+}
+
+
+const Accordion = (props:AccordionPropsType) => {
+    console.log('accordion rendering')
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+            { !props.collapsed && <AccordionBody/>}
+        </div>
+    );
+};
+
+type AccordionTitlePropsType ={
+    title: string
+}
+
+function AccordionTitle(props:AccordionTitlePropsType) {
+    console.log('AccordionTitle rendering')
+    return (
+        <h3>--{props.title}--</h3>
+    )
+}
+
+function AccordionBody (){
+    console.log('accordion rendering')
+    return(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+
+    )
+}
+
+export default Accordion;
