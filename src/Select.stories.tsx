5f3155cf0266662925b7f90b4620b8eb80603ad2
Alex <alex.alexandrov.1988@gmail.com>
2023-12-12 17:13:48 +0300

    lesson 14 storybook

---
+++ src/Select.stories.tsx
@@ -0,0 +1,17 @@
+import type {Meta} from '@storybook/react';
+
+import Select, {RatingValueType} from './Rating';
+import {useState} from "react";
+
+const meta: Meta<typeof Select> = {
+    component: Select,
+};
+
+export default meta;
+
+export const SelectBase =()=> <Select value={'1'} onChange={action('adsfasdf')} items={[
+    {value"1", title:'minsk'},
+    {value"2", title:'minsk'},
+    {value"3", title:'minsk'},
+    {value"4", title:'minsk'},
+]}/>
